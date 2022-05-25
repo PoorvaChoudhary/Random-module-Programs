44c30cb050bba6e4b33135b9012b9ecdc376c19b
Poorva Chaudhary <106022734+PoorvaChoudhary@users.noreply.github.com>
2022-05-25 15:32:21 +0530

    Add files via upload

---
+++ Multiplication of two random numbers.py
@@ -0,0 +1,11 @@
+# Calculate multiplication of two random float numbers
+
+import random
+
+num1  = random.random()
+print("First Random float is ", num1)
+num2 = random.uniform(9.5, 99.5)
+print("Second Random float is ", num1)
+
+num3 = num1 * num2
+print("Multiplication is ", num3)
