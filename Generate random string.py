c07315257dbe023c9baa87302e6902f43b636198
Poorva Chaudhary <106022734+PoorvaChoudhary@users.noreply.github.com>
2022-05-25 15:32:41 +0530

    Add files via upload

---
+++ Generate random string.py
@@ -0,0 +1,11 @@
+# Generate random String of length 5
+
+import random
+import string
+
+def randomString(stringLength):
+    """Generate a random string of 5 charcters"""
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+print ("Random String is ", randomString(5) )
